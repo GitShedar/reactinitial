e5082a752b76760cb20b303656fa808da30a187d
Angela Csomo <angela.csomo@gmail.com>
2021-03-24 06:45:23 +0100

    try

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import './App.css'
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import LoadingMask from "./components/LoadingMask";
+import Hotels from "./components/Hotels";
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+
+    fetch("./api/hotels")
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setData(null))
+      .finally(() => setIsLoading(true));
+  }, []);
+
+  console.log(data);
 
   return (
     <div className="App">
-      Todo...
-    </div>
-  )
-}
+      <h1>Hotels</h1>
 
-export default App
+      {!isLoading ? (
+        <LoadingMask />
+      ) : (
+        data.map((item) => <Hotels key={item.id} proptohotels={item} />)
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Hotels.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Hotels(props) {
+  return (
+    <div>
+      {props.proptohotels.name}
+    </div>
+  )
+}
+
+export default Hotels
